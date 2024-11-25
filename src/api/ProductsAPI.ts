8952a11f444089c529b0814906f4ea0c84d35c42
MilagrosLuque <luquemariamilagros@gmail.com>
2024-11-25 15:51:03 -0300

    fixxed url products

--- src/api/ProductsAPI.ts
+++ src/api/ProductsAPI.ts
@@ -3,7 +3,7 @@ import Swal from "sweetalert2";
 
 export const fetchDataProducts = async (): Promise<IProduct[]> => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
+    const res = await fetch(`https://drc116rn-3002.brs.devtunnels.ms/products`, {
       next: { revalidate: 1200 },
     })
     if (!res.ok) {
